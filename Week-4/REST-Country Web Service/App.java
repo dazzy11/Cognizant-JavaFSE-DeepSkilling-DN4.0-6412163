0b746d71e73781ce819c009fbf084eb527594400
dazzy11 <deepakmadhukumar1124@gmail.com>
2025-08-13 21:15:52 +0530

    updated

--- Week-4/REST-Country Web Service/App.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.springlearn.model;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
--- Week-6/handsOn_1/myfirstreactapp/src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-    <h1> welcome to the first session of React</h1>
-  );
-}
-
-export default App;
